f4cc1e247715294e64ecfed54d2cbcd5934e2c4e
Felipe Bessa Coelho <fcoelho@localhost>
2018-09-06 12:59:01 -0300

    v32

--- server.go
+++ server.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	handler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "v31\n")
+		io.WriteString(w, "v32\n")
 	}
 
 	http.HandleFunc("/", handler)
