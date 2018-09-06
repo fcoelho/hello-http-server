07377918be98e9945c8616bf88a1c0848441dc24
Felipe Bessa Coelho <fcoelho@localhost>
2018-09-06 18:49:53 -0300

    v84

--- server.go
+++ server.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	handler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "v83\n")
+		io.WriteString(w, "v84\n")
 	}
 
 	http.HandleFunc("/", handler)
